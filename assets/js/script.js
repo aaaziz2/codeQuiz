b607f183cf6d5d7128a07a665f74e573fb11a0d6
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-06 09:32:02 -0500

    added event listener for selecting answers added function to generate the questions and answers added the li to index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,37 @@
 // Select Elements from the DOM
 var startButton = document.querySelector("#start")
 var questions = document.querySelector("#intro")
-var answers = document.querySelector("#introP")
+var skip = document.querySelector("#introP")
+var choices = document.querySelector("#choices")
 
 // Global Variables
-var questionsArr = []
-var answersArr = []
+var questionsArr = ["What language do you use (primarily) to add styles to HTML elements?",
+                    "Question 2",
+                    "Question 3",
+                    "Question 4,"]
+var answersArr = ["CSS","JavaScript","PHP","Swift",]
 var timer = 75
 var correct = 0
+var current = 0
+var choiceCounter = 0
 
 var timerShow = document.createElement("p")
 timerShow.textContent = timer
 
+
+
 startButton.addEventListener("click",function() {
-    questions.textContent = "Question 1"
-    answers.textContent = "Yes or no?"
+    
+    skip.textContent = ""
     startButton.setAttribute("style","display:none")
     correct = 0
+
+    generateQuestion()
     
     document.body.children[0].appendChild(timerShow)
-    console.log(document.body.children[0].children[1])
     startTimer()
 })
+
 
 function startTimer(){
     var timerInterval = setInterval(function(){
@@ -36,16 +46,44 @@ function startTimer(){
     },1000)
 }
 
+choices.addEventListener("click",function(event){
+    var selection = event.target
+    var ans = selection.getAttribute("data-id")
+    
+    if(ans == 1){
+        correct++
+    }
+    else{
+        timer-=10
+    }
+    generateQuestion()
+})
+
+function generateQuestion(){
+    questions.textContent = questionsArr[current]
+    current++
+    
+    for(var j = 0; j<4; j++){
+        choices.children[j].textContent = answersArr[choiceCounter+j]
+    }
+    
+    choiceCounter+=4
+}
+
+
 // PseudoCode for Quiz Game
 
-// Start page will have explanation of the game
+// Start page will have explanation of the game ...Done!
 // Pressing the Start button will load questions
-// Timer starts on start button being pressed
-// A random question will be generated 
+    // Easiest way is for H1 to host questions
+    // How to randomize questions without Duplicates?
+    // lets google/ I don't think I need to
+// Timer starts on start button being pressed ...Done!
+// A question will be generated 
 // A set of 4 choices will be loaded 
 // (Their placement in the list will be random)
 // Only one correct answer in the 4 choices
 // When you select an answer the next one is loaded until the timer hits 0
 // If you get a correct answer that is 
 // If you get a wrong answer you are penalized 10 seconds on the clock
-// Once the clock hits 0 the highscore page is loaded for you to enter your name in to the scoreboard
+// Once the clock hits 0 the highscore page is loaded for you to enter your name in to the scoreboard ...Done!
