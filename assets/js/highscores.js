caffbdeb11461d6247efafddeedc848e47612b55
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-07 18:10:26 -0500

    Added more comments throughout Added the ability to print all items from localStorage for highscores Added a check for the same name warning of the overwrite

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,36 +1,49 @@
+// Grab the element from the HTML we need to manipulate
 var form = document.querySelector("#hs-form")
 var input = document.querySelector("#hs-input")
 var leaderboard = document.querySelector("#leaderboard")
 var back = document.querySelector("#back")
 
-// var winners = [
-//             {name: "", score: "", id:"num1"},
-//             {name: "", score: "", id:"num2"},
-//             {name: "", score: "", id:"num3"}
-//         ]
 
 function addScore(event){
+    // https://www.javascripttutorial.net/web-apis/javascript-localstorage/
+    // get list of names from localStorage
+    let keys = Object.keys(localStorage)
+    //make sure the screen doesn't refresh on submit
     event.preventDefault()
 
-    if(!input){
+    // Make sure there isn't a blank entry
+    if(!input.value){
         alert("Please Enter a Name!")
         return
+    }
+    // Warning user that if they enter a value again the will overwrite the existing score
+    else if(keys.includes(input.value) && (!confirm("Are you sure? This will overwrite your last score"))){
+            return        
     }   
-
-    // localStorage.setItem(input.value,localStorage.getItem("score"))
+    
+    // Remove Highscore Related Elements
     form.setAttribute("style","display:none")
     document.querySelectorAll('h3')[0].setAttribute("style","display:none")
 
-    var newScore = document.createElement('li')
-    newScore.textContent = `${input.value} got a ${localStorage.getItem('score')}`
+    //write score to localstorage associated with the user's name
+    localStorage.setItem(input.value,localStorage.getItem('score'))
+    // removed score so that it doesn't show up in the highscores
+    localStorage.removeItem('score')
 
-    leaderboard.appendChild(newScore)
+    // needs to get the new keys so score doesn't show up
+    keys = Object.keys(localStorage)
 
-
+    // prints everything in localStorage
+    for(i = 0; i< keys.length; i++){
+        var newScore = document.createElement('li')
+        newScore.textContent = `${keys[i]} got a ${localStorage.getItem(keys[i])}`
+        leaderboard.appendChild(newScore)
+    }
 
 }
 
-
+// When form submits add user to Highscores
 form.addEventListener("submit",addScore)
 
 // Back button to take the quiz again
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ var choices = document.querySelector("#choices")
 var questionsArr = ["What language do you use (primarily) to add styles to HTML elements?",
                     "What does CSS stand for?",
                     "Where in an HTML Document is the correct place to refer to an external style sheet?",
-                    "Question 4,"]
+                    "Where in an HTML Document is the correct place to refer to a Javascript Script,"]
 var answersArr = ["CSS","JavaScript","PHP","Swift",
                 "Cascading Style Sheets","Colorful Style Sheets","Castiron Steel Stock","Computer Style Sheets",
                 "In the <head>","In the <body>","At the end of document","Right After DOCTYPE",
-                "Ans 1","Ans 2","Ans 3","Ans 4"]
+                "After the <body>","Top of the <body>","In the <head>","Right After DOCTYPE"]
 var timer = 40
 var correct = 0
 var current = 0
@@ -23,17 +23,19 @@ var timerShow = document.createElement("p")
 timerShow.textContent = timer
 
 
-// start button gets rid of intro page material and starts generating questions
-// resets the counter so you can play again and get different scores
+// Set up the page for the quiz
 startButton.addEventListener("click",function() {
-    
+    // Removing Intro Elements
     skip.textContent = ""
     startButton.setAttribute("style","display:none")
+
+    // Reseting the variables so if you start the quiz again everything works
     correct = 0
     choiceCounter = 0
-
+    
     generateQuestion()
     
+    // Add timer to the screen
     document.body.children[0].appendChild(timerShow)
     startTimer()
 })
@@ -56,6 +58,7 @@ function startTimer(){
 // Checks user selection to see if the answer is correct, if not it will take time off the clock
 choices.addEventListener("click",function(event){
     var selection = event.target
+
     var ans = selection.getAttribute("data-id")
     
     if((ans%4) == 0){
@@ -67,22 +70,28 @@ choices.addEventListener("click",function(event){
     generateQuestion()
 })
 
-// Stores the score into localStorage, when you run out of questions it will load the highscores page
 // Generates the questions and answers from the corresponding arrays and adds them to the page
 // I want to implement a way to randomize the order of choices but haven't gotten to that yet
 function generateQuestion(){
+    // write scores to localStorage incase we are out of questions
     localStorage.setItem("score",correct)
+    //  when you run out of questions it will load the highscores page
     if(!questionsArr[current]){
         return window.location = "highscores.html"
     }
+    // Generate the question
     questions.textContent = questionsArr[current]
+    // Increment counter so it will load the next question when the function is run again
     current++
     
+    // Generate the possible choices
     for(var j = 0; j<4; j++){
+
         choices.children[j].textContent = answersArr[choiceCounter+j]
         choices.children[j].setAttribute("data-id",choiceCounter+j)
     }
     
+    // Increment counter so it will load the next 4 choices when the function is run again
     choiceCounter+=4
 }
 
