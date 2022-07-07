15a16c89e7c9ba027a38dd638e68365e769b523e
Aumio Aziz <aumio.aziz@gmail.com>
2022-07-07 08:19:21 -0500

    Added writing score to local storage on timeout or when quiz finished Changed how to check correct scores Added going to highscores when questions finished Added js page for highscores page Added back button for highscores page

---
+++ assets/js/highscores.js
@@ -0,0 +1,18 @@
+var form = document.querySelector("hs-form")
+var input = document.querySelector("hs-input")
+
+var participent = []
+
+function addScore(event){
+    event.preventDefault()
+
+    if(input == ''){
+        alert("Please Enter a Name!")
+        return
+    }   
+
+    // var 
+}
+
+
+form.addEventListener('submit',addScore)
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,13 @@ var choices = document.querySelector("#choices")
 
 // Global Variables
 var questionsArr = ["What language do you use (primarily) to add styles to HTML elements?",
-                    "Question 2",
-                    "Question 3",
+                    "What does CSS stand for?",
+                    "Where in an HTML Document is the correct place to refer to an external style sheet?",
                     "Question 4,"]
-var answersArr = ["CSS","JavaScript","PHP","Swift",]
+var answersArr = ["CSS","JavaScript","PHP","Swift",
+                "Cascading Style Sheets","Colorful Style Sheets","Castiron Steel Stock","Computer Style Sheets",
+                "In the <head>","In the <body>","At the end of document","Right After DOCTYPE",
+                "Ans 1","Ans 2","Ans 3","Ans 4"]
 var timer = 75
 var correct = 0
 var current = 0
@@ -25,6 +28,7 @@ startButton.addEventListener("click",function() {
     skip.textContent = ""
     startButton.setAttribute("style","display:none")
     correct = 0
+    choiceCounter = 0
 
     generateQuestion()
     
@@ -40,6 +44,7 @@ function startTimer(){
         if(timer === 0){
             setTimeout(function(){
                 clearInterval(timerInterval)
+                localStorage.setItem("score",correct)
                 return window.location = "highscores.html"
             })
         }
@@ -50,7 +55,7 @@ choices.addEventListener("click",function(event){
     var selection = event.target
     var ans = selection.getAttribute("data-id")
     
-    if(ans == 1){
+    if((ans%4) == 0){
         correct++
     }
     else{
@@ -60,11 +65,16 @@ choices.addEventListener("click",function(event){
 })
 
 function generateQuestion(){
+    localStorage.setItem("score",correct)
+    if(!questionsArr[current]){
+        return window.location = "highscores.html"
+    }
     questions.textContent = questionsArr[current]
     current++
     
     for(var j = 0; j<4; j++){
         choices.children[j].textContent = answersArr[choiceCounter+j]
+        choices.children[j].setAttribute("data-id",choiceCounter+j)
     }
     
     choiceCounter+=4
